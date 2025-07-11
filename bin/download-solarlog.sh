09a7f7b372e19959dc48835fdaa44775ed0985ea
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-07-11 13:08:50 +0200

    Add cachedir to .sh script

--- bin/download-solarlog.sh
+++ bin/download-solarlog.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 . ./.env # <-- Put SOLARLOG_TOKEN=<TOKEN> in this file
+CACHE_DIR="./.cache/"
 
 for year in 2024 2025; do
     for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
@@ -8,7 +9,7 @@ for year in 2024 2025; do
         curl -H "Authorization: Bearer $SOLARLOG_TOKEN" \
             "$SOLARLOG_BASE_URL/visualization/plant/$SOLARLOG_PLANT_ID/channels?dateFrom=$year-$month-01&dateTo=$year-$month-31&channelNames%5B%5D=ProdPac" \
             | jq \
-            > ./$SOLARLOG_OUT_DIR/$year-$month.json
+            > ./$CACHE_DIR/$year-$month.json
     done
     echo "$year done!"
 done
